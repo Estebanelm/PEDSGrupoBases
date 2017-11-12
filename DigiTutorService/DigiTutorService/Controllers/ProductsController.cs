c8e59e313e0b1dff4cc1d816418c7c7a935a1d00
DESKTOP-A4GNH4T\Pelo <estebanedcv@gmail.com>
2017-11-12 15:01:42 -0600

    Agregué el proyecto de servicio RESTful con los métodos get, post y put funcionales.

---
+++ DigiTutorService/DigiTutorService/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using DigiTutorService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DigiTutorService.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        List<Product> products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        }.ToList();
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+        [HttpGet]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        [HttpPost]
+        public IHttpActionResult PostProduct([FromBody] Product model)
+        {
+            products.Add(model);
+            return Ok();
+        }
+        [HttpPut]
+        public IHttpActionResult ModifyProduct([FromBody] Product model)
+        {
+            var productoAModificar = products.Where(prod => prod.Id.Equals(model.Id)).FirstOrDefault();
+            if (productoAModificar != null)
+            {
+                products.Remove(productoAModificar);
+                products.Add(model);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ DigiTutorService/DigiTutorService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DigiTutorService.WebApiApplication" Language="C#" %>
