f1b5c663049cb5970394e6beeef66ebb6f25fbcd
h4koo <georgeaz56@gmail.com>
2017-11-26 10:37:58 -0600

    Intento de commit, agregar Modelos y objetos de la capade acceso a datos

---
+++ DigiTutorService/DigiTutorService/Models/Administrador.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DigiTutorService.Models
+{
+    public class Administrador
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
