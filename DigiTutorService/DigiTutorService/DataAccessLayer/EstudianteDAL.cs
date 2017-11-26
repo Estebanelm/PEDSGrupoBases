761d5d0b3f005cf0c7b75aa355cd05493345ef6c
h4koo <georgeaz56@gmail.com>
2017-11-26 09:37:27 -0600

    PrimerDAL (prueba git)

---
+++ DigiTutorService/DigiTutorService/DataAccessLayer/EstudianteDAL.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DigiTutorService.DataAccessLayer
+{
+    public class EstudianteDAL
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    
+}
