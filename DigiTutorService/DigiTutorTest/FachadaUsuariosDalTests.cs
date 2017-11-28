01e8d29391fe65e2751ef461020d03480ee471a5
Pelo <estebanedcv@gmail.com>
2017-11-28 16:31:52 -0600

    Probé un test simple y lo agregué a la solución.

--- DigiTutorService/DigiTutorTest/FachadaUsuariosDalTests.cs
+++ DigiTutorService/DigiTutorTest/FachadaUsuariosDalTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DigiTutorService.DataAccessLayer;
+using DigiTutorService.Models;
+using System.Linq;
 
 namespace DigiTutorTest
 {
@@ -7,8 +10,29 @@ namespace DigiTutorTest
     public class FachadaUsuariosDalTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCrearEstudiante()
         {
+            FachadaUsuariosDAL testObject = new FachadaUsuariosDAL();
+            string password = "123asdlk";
+            Estudiante estudiante = new Estudiante
+            {
+                Nombre = "Esteban",
+                Apellido = "Calvo",
+                Universidad = "ITCR",
+                Pais = "Costa Rica",
+                Tecnologias = new Estudiante.TecnologiaPerfil[]
+                    {
+                        new Estudiante.TecnologiaPerfil{Nombre = "Calculo", Apoyos = 0, MiApoyo = "nunca"},
+                        new Estudiante.TecnologiaPerfil{Nombre = "Calculo", Apoyos = 0, MiApoyo = "nunca"}
+                    }.ToList(),
+                Telefono = "8888-8888",
+                Correo = "estebancalvo@hotmail.com",
+                FechaInscripcion = DateTime.Now,
+                Descripcion = "muchacho trabajador"
+            };
+            bool expected = true;
+            bool actual = testObject.CrearEstudiante(password, estudiante);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
