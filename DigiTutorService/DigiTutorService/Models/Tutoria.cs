737443ae8a824947b8529d71d3b2e4efb8936acb
Pelo <estebanedcv@gmail.com>
2017-11-30 00:01:28 -0600

    Arreglé la serialización de las publicaciones

--- DigiTutorService/DigiTutorService/Models/Tutoria.cs
+++ DigiTutorService/DigiTutorService/Models/Tutoria.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace DigiTutorService.Models
 {
+    [KnownType(typeof(Publicacion))]
+    [DataContract]
     public class Tutoria : Publicacion
     {
+        [DataMember]
         public string Costo { get; set; }
+        [DataMember]
         public string Lugar { get; set; }
+        [DataMember]
         public bool EstoyRegitrado { get; set; }
+        [DataMember]
         public DateTime FechaTutoria { get; set; }
 
     }
