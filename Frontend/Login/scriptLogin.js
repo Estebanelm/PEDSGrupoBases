a215fe885d8e5250eb181b8fd4c08a67c0ea7371
Steven <maiderr97@gmail.com>
2017-12-01 00:54:18 -0600

    Se añadio el script de angular a la pagina de Login

---
+++ Frontend/Login/scriptLogin.js
@@ -0,0 +1,45 @@
+﻿var app = angular.module('loginApp', []);
+
+app.controller('loginCtrl', function ($scope, $http, $filter, $location, $window) {
+
+
+    $scope.formulario = {};
+    $scope.login = {};
+    $scope.serverURL = "http://186.176.172.50/DigiTutor/api/";
+    $scope.ID;
+    $scope.Password;
+
+    $scope.BuildLogin = function (p_formulario) {
+        newObj = {};
+        newObj.id_estudiante = $scope.formulario.ID;
+        newObj.contrasena = $scope.formulario.Password;
+        return newObj;
+
+
+    };
+
+    $scope.SubmitForm = function () {
+        $scope.login = $scope.BuildLogin($scope.formulario);
+        url = $scope.serverURL + "login";
+        jsonLogin = JSON.stringify($scope.login);
+
+        //post
+        $http.post(url, jsonLogin).then(function (response) {
+            window.setTimeout(function () {
+
+                // Move to a new location or you can do something else
+                window.location = "http://186.176.172.50/DigiTutor/";
+
+            }, 2000);
+
+
+        },
+            function (response) {
+
+            });
+    }
+
+
+
+
+});
